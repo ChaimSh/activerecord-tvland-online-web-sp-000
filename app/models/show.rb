a579cac17726d7df452c3a01688663bc4d0d8b7a
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-04 18:44:05 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
   def actors_list
    #Returns an Array of the full names of each actor
    #associated with the a show.
-    full_name = self.first_name + " " +self.last_name
+    full_name = self.first_name + " " + self.last_name
     full_name
   end
 end
