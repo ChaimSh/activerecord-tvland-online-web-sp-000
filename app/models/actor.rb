6dcc0805d7bfce349ff153d69372b61572e5f6a8
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-04 17:48:32 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
 
   def list_roles
     # can list all of its shows and characters
-    self.characters + " - " + self.shows
+    self.characters  " - "  self.shows
 
   end
 
