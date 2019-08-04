4211570f897b69c1b20cfd39544c97764d20f50e
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-04 17:31:56 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    # can list all of its shows and characters
+  end
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+
+  def actors_list
+   #Returns an Array of the full names of each actor
+   #associated with the a show.
+
+  end
 end
