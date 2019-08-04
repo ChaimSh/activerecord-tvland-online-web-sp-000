c0d47aa74c9de7e3e8b7f0f5a7a07108962f1fdb
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-08-04 18:48:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,9 @@ class Show < ActiveRecord::Base
   def actors_list
    #Returns an Array of the full names of each actor
    #associated with the a show.
-    full_name = self.first_name + " " + self.last_name
-    full_name
+
+    actors = []
+    actors << Actor.full_name 
+    actors
   end
 end
